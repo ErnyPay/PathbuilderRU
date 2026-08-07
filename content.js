632e4ff27acd0b8b83ff68e904b92c63296aa4ec
ErnyPay <nikitkabaranov1664@gmail.com>
2026-08-07 20:03:36 +0300

    1

--- content.js
+++ content.js
@@ -2,7 +2,7 @@
 
 (async () => {
 
-    console.log('[PathbuilderRU] Loading dictionary...');
+    console.log('[PathbuilderRU] Starting...');
 
     const response = await fetch(
         chrome.runtime.getURL('dictionaries/ui.json')
@@ -10,6 +10,66 @@
 
     const dictionary = await response.json();
 
-    console.log(dictionary);
+    function translateText(node) {
+
+        if (!node.nodeValue) return;
+
+        const text = node.nodeValue.trim();
+
+        if (dictionary[text]) {
+
+            console.log("Перевод:", text, "->", dictionary[text]);
+
+            node.nodeValue = dictionary[text];
+
+        }
+
+    }
+
+    function translateElement(root) {
+
+        if (!root) return;
+
+        const walker = document.createTreeWalker(
+            root,
+            NodeFilter.SHOW_TEXT
+        );
+
+        while (walker.nextNode()) {
+
+            translateText(
+                walker.currentNode
+            );
+
+        }
+
+    }
+
+    translateElement(document.body);
+
+    const observer = new MutationObserver((mutations)=>{
+
+        for(const mutation of mutations){
+
+            mutation.addedNodes.forEach(node=>{
+
+                if(node.nodeType===1){
+
+                    translateElement(node);
+
+                }
+
+            });
+
+        }
+
+    });
+
+    observer.observe(document.body,{
+        childList:true,
+        subtree:true
+    });
+
+    console.log("[PathbuilderRU] Translator ready");
 
 })();
