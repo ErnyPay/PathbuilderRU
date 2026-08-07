5fd55bee96b0d57d40b683e2c7cd23bfb98d0a45
ErnyPay <nikitkabaranov1664@gmail.com>
2026-08-07 19:55:28 +0300

    Install001

--- content.js
+++ content.js
@@ -1,15 +1,5 @@
 'use strict';
 
-(async () => {
+console.log('[PathbuilderRU] Extension loaded');
 
-    console.log('[PathbuilderRU] Starting...');
-
-    await Dictionary.load();
-
-    Translator.translateElement(document.body);
-
-    Observer.start();
-
-    console.log('[PathbuilderRU] Ready');
-
-})();
+alert('PathbuilderRU работает!');
